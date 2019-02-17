06513e7265f47b341f9b195d5901ed35f7ddefd4
nesosuke <nesosuke@gmail.com>
2019-02-18 00:28:25 +0900

    SKIP_DEPLOYMENTなんとかを追記

--- mastodon-update-script.sh
+++ mastodon-update-script.sh
@@ -7,37 +7,36 @@
 ###
 
 ### Conf values 
-. ~/access_token
+export $(cat ~/access_token)
+SKIP_POST_DEPLOYMENT_MIGRATIONS=true
 
 ### Def. func toot ###
-function toot () { curl -X POST -d "access_token=$ACCESS_TOKEN&status=$1" -Ss https://$DOMAIN/api/v1/statuses ; }
+#function toot () { curl -X POST -d "access_token=$ACCESS_TOKEN&status=$1" -Ss https://$DOMAIN/api/v1/statuses ; }
 
 ### Update pkg(s) 
-toot ":neso: < update script is starting... at $(date)"
 cd ~/live 
 sudo apt update -y 
 sudo apt upgrade -y 
 cd ~/.rbenv/plugins/ruby-build && git pull && cd -
+gem update --system
 gem install bundler 
 bundle install 
 yarn install 
 
 ### Migrate  
 RAILS_ENV=production bundle exec rails assets:clobber 
-toot ":neso: < migration starting at $(date)"
 RAILS_ENV=production bundle exec rails db:migrate 
 
+sudo systemctl restart mastodon-*.service 
+
 ### Precompile
-toot ":neso: < precompiling started at $(date)" 
 RAILS_ENV=production bundle exec rails assets:precompile 
-toot ":neso: < precompiling finished at $(date)" 
+RAILS_ENV=production bundle exec rails db:migrate  
 
 ### Restart mastodon-*.service ###
 sudo systemctl restart mastodon-*.service nginx
-sleep 5 && toot ":neso: < services restarting..." 
 
-### Toot the end of script ###
-toot ":neso: < The update script is ending at $(date)"
+
 
 
 # EOF
